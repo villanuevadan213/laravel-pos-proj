f9b92b1960e1383ee7f72c461f8ec280ea04302c
Dan Louie <villanuevadan213@gmail.com>
2025-03-29 00:46:14 +0800

    navigation links

---
+++ resources/views/inventory.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:heading>
+        Inventory Page
+    </x-slot:heading>
+
+    <h1>Hello from the Inventory Page</h1>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/inventory', function () {
+    return view('inventory');
+});
+
+Route::get('/sales', function () {
+    return view('sales');
+});
+
+Route::get('/pos', function () {
+    return view('pos');
 });
