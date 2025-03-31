569768f7d262c757546a15079a45c61482bea161
Dan Louie <villanuevadan213@gmail.com>
2025-04-01 05:11:17 +0800

    Routes essentials

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -118,7 +118,7 @@
         </nav>
 
         <header class="bg-white shadow-sm">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex sm:justify-between">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex justify-between">
                 <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
 
                 <x-button href="/items/create">Create Item</x-button>
--- routes/web.php
+++ routes/web.php
@@ -1,82 +1,14 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Item;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/dashboard', 'dashboard');
+Route::view('/sales', 'sales');
+Route::view('/pos', 'pos');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
-
-Route::get('/items', function ()  {
-    $items = Item::with('supplier')->latest()->simplePaginate(3);
-
-    return view('items.index', [
-        'items' => $items
-    ]);
-});
-
-Route::get('/items/create', function ()  {
-    return view('items.create');
-});
-
-Route::get('/items/{id}', function ($id)  {
-    $item = Item::find($id);
-
-    return view('items.show', ['item' => $item]);
-});
-
-Route::post('/items', function () {
-    request()->validate([
-        'name' => ['required', 'min:3'],
-        'price' => ['required']
-    ]);
-
-    Item::create([
-        'name' => request('name'),
-        'price' => request('price'),
-        'supplier_id' => 1
-    ]);
-
-    return redirect('/items');
-});
-
-// Edit Item
-Route::get('/items/{id}/edit', function ($id)  {
-    $item = Item::find($id);
-
-    return view('items.edit', ['item' => $item]);
-});
-
-Route::patch('/items/{id}', function ($id)  {
-    request()->validate([
-        'name' => ['required', 'min:3'],
-        'price' => ['required']
-    ]);
-
-    $item = Item::findOrFail($id);
-
-    $item->update([
-        'name' => request('name'),
-        'price' => request('price'),
-    ]);
-
-    return redirect('/items/' . $item->id);
-});
-
-Route::delete('/items/{id}', function ($id)  {
-    Item::findOrFail($id)->delete();
-
-    return redirect('/items');
-});
-
-Route::get('/sales', function () {
-    return view('sales');
-});
-
-Route::get('/pos', function () {
-    return view('pos');
-});
+Route::resource('items', ItemController::class); 
+// 7 Basic function index, show, create, store, edit, update, destroy
+// use ['only' => '' / 'except' => ''] to include or exclude
